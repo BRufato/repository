009e60a6335ca5c029a7691e2dfa3b260de5b5aa
BRufato <brunorufato31@gmail.com>
2025-08-27 12:14:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -10,12 +9,4 @@ registerLink.addEventListener('click', ()=> {
 
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
 });
